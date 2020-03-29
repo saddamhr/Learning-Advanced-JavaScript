5449eb6e12431db82df5935fbaf74ab0b95080d1
saddamhr <saddambubt65@gmail.com>
2020-03-29 18:50:43 +0600

    purpose of event bubble

--- event-bubble/event-bubble.js
+++ event-bubble/event-bubble.js
@@ -1,10 +1,25 @@
-document.getElementById('first').addEventListener('click', function(event) {
-    console.log('First item clicked')
-    event.stopPropagation();
+// var items = document.getElementsByClassName('item');
+// for (let i = 0; i < items.length; i++) {
+//     const item = items[i];
+//     item.addEventListener('click', function(event) {
+//         event.target.parentNode.removeChild(event.target);
+//     })
+    
+// }
+
+document.getElementById('addNew').addEventListener('click', function(event) {
+    var itemParent = document.getElementById('myList');
+    var newItem = document.createElement('li');
+    newItem.innerHTML = "Brand New Item";
+    itemParent.appendChild(newItem);
 })
+// document.getElementById('first').addEventListener('click', function(event) {
+//     console.log('First item clicked')
+//     event.stopPropagation();
+// })
 document.getElementById('myList').addEventListener('click', function(event) {
-    console.log('UL clicked')
+    event.target.parentNode.removeChild(event.target);
 })
-document.getElementById('container').addEventListener('click', function(event) {
-    console.log('Container clicked')
-})
+// document.getElementById('container').addEventListener('click', function(event) {
+//     console.log('Container clicked')
+// })
