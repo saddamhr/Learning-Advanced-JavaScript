bf9fe7bf35cd47369626b680ad458f5367e38cef
saddamhr <saddambubt65@gmail.com>
2020-03-29 13:00:46 +0600

    return from function

---
+++ return-from-function.js
@@ -0,0 +1,24 @@
+function evenOdd(number) {
+    var result;
+    if( number % 2 == 0) {
+        result = number;
+    } else {
+        result = number * 2;
+    }
+    return result;
+}
+
+function evenOddAll (numbers) {
+    var evenArray = [];
+    for (let i = 0; i < numbers.length; i++) {
+        const number = numbers[i];
+        var result = evenOdd(number);
+        evenArray.push(result)
+
+    }
+    return evenArray;
+}
+
+numbers = [1,2,3,4,5];
+var numbers =  evenOddAll(numbers)
+console.log(numbers)
