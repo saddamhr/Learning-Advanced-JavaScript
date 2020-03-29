95f23074bb1d51e50b64186a5f31aa3b4736ee80
saddamhr <saddambubt65@gmail.com>
2020-03-29 13:51:09 +0600

    event bubble with example

---
+++ event-bubble/event-bubble.js
@@ -0,0 +1,9 @@
+document.getElementById('first').addEventListener('click', function(event) {
+    console.log('First item clicked')
+})
+document.getElementById('myList').addEventListener('click', function(event) {
+    console.log('UL clicked')
+})
+document.getElementById('container').addEventListener('click', function(event) {
+    console.log('Container clicked')
+})
